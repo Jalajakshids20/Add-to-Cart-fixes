f7b09726c052b4f2a2d00d0da56f83edb5360697
Jalajakshi D S <114593975+Jalajakshids20@users.noreply.github.com>
2023-12-14 21:57:42 +0530

    Add files via upload

---
+++ CartContext.js
@@ -0,0 +1,52 @@
+import React, { createContext, useContext, useReducer } from 'react';
+
+
+const CartContext = createContext();
+
+// Initial state for the cart
+const initialState = {
+  cartItems: [],
+};
+
+// Reducer function to manage cart actions
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TO_CART':
+      return {
+        ...state,
+        cartItems: [...state.cartItems, action.payload],
+      };
+    case 'REMOVE_FROM_CART':
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(item => item.id !== action.payload),
+      };
+    case 'CLEAR_CART':
+      return {
+        ...state,
+        cartItems: [],
+      };
+    default:
+      return state;
+  }
+};
+
+// Custom hook to use the cart context
+export const useCart = () => {
+  const context = useContext(CartContext);
+  if (!context) {
+    throw new Error('useCart must be used within a CartProvider');
+  }
+  return context;
+};
+
+// CartProvider component to wrap 
+export const CartProvider = ({ children }) => {
+  const [cartState, dispatch] = useReducer(cartReducer, initialState);
+
+  return (
+    <CartContext.Provider value={{ cartState, dispatch }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
